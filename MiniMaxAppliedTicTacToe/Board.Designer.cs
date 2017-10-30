c481398e626666b22d5fab22329acf52109c015e
JADENLAPTOP\Jaden <emailtojaden@gmail.com>
2017-10-30 14:22:29 -0400

    Initial commit

---
+++ MiniMaxAppliedTicTacToe/Board.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace MiniMaxAppliedTicTacToe
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.cBoxPlayer = new System.Windows.Forms.ComboBox();
+            this.checkAI = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(10, 13);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(80, 95);
+            this.btn0.TabIndex = 4;
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn1.Location = new System.Drawing.Point(95, 13);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(80, 95);
+            this.btn1.TabIndex = 5;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn2.Location = new System.Drawing.Point(180, 13);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(80, 95);
+            this.btn2.TabIndex = 6;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn3.Location = new System.Drawing.Point(10, 115);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(80, 95);
+            this.btn3.TabIndex = 7;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn4.Location = new System.Drawing.Point(95, 115);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(80, 95);
+            this.btn4.TabIndex = 8;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn5.Location = new System.Drawing.Point(180, 115);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(80, 95);
+            this.btn5.TabIndex = 9;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn6.Location = new System.Drawing.Point(10, 217);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(80, 95);
+            this.btn6.TabIndex = 10;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn7.Location = new System.Drawing.Point(95, 217);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(80, 95);
+            this.btn7.TabIndex = 11;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Bold);
+            this.btn8.Location = new System.Drawing.Point(180, 217);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(80, 95);
+            this.btn8.TabIndex = 12;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(10, 358);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(110, 35);
+            this.btnInit.TabIndex = 2;
+            this.btnInit.Text = "Restart";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // cBoxPlayer
+            // 
+            this.cBoxPlayer.FormattingEnabled = true;
+            this.cBoxPlayer.Items.AddRange(new object[] {
+            "Player 1 (O)",
+            "Player 2 (X)"});
+            this.cBoxPlayer.Location = new System.Drawing.Point(70, 322);
+            this.cBoxPlayer.Name = "cBoxPlayer";
+            this.cBoxPlayer.Size = new System.Drawing.Size(92, 21);
+            this.cBoxPlayer.TabIndex = 0;
+            this.cBoxPlayer.SelectedIndexChanged += new System.EventHandler(this.cBoxPlayer_SelectedIndexChanged);
+            // 
+            // checkAI
+            // 
+            this.checkAI.AutoSize = true;
+            this.checkAI.Checked = true;
+            this.checkAI.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkAI.Location = new System.Drawing.Point(189, 325);
+            this.checkAI.Name = "checkAI";
+            this.checkAI.Size = new System.Drawing.Size(71, 17);
+            this.checkAI.TabIndex = 1;
+            this.checkAI.Text = "Versus AI";
+            this.checkAI.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Play As: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 407);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkAI);
+            this.Controls.Add(this.cBoxPlayer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Name = "Board";
+            this.Text = "Unbeatable Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Board_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.ComboBox cBoxPlayer;
+        private System.Windows.Forms.CheckBox checkAI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MiniMaxAppliedTicTacToe/Board.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniMaxAppliedTicTacToe
+{
+    public partial class Board : Form
+    {
+        public Board()
+        {
+            InitializeComponent();
+        }
+
+        const int BOARD_SIZE = 9;
+        string[] board = new string[BOARD_SIZE];
+        int playerIndex = 0;
+        string[] player = { "O", "X" };
+        int totalNumber = 0;
+        double[] scoreIndex = new double[BOARD_SIZE];
+
+        enum gamePhase
+        {
+            gameStart,
+            gameOver
+        }
+        gamePhase phase = new gamePhase();
+
+        void initialize()
+        {
+            playerIndex = 0;
+            cBoxPlayer.SelectedIndex = 0;
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board[i] = i.ToString();
+            }
+
+            //Find all button named "btn" + i and set the text to ""
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                Button btn = this.Controls.Find("btn" + i, false).FirstOrDefault() as Button;
+                btn.Text = "";
+            }
+
+            phase = gamePhase.gameStart;
+            if (playerIndex == 1)
+            {
+                //Here comes AI
+                int aiMove = minimax(playerIndex);
+                if (aiMove != -1)
+                {
+                    board[aiMove] = player[playerIndex];
+                    Button btn = this.Controls.Find("btn" + aiMove, false).FirstOrDefault() as Button;
+                    btn.Text = player[playerIndex];
+                    playerIndex = 1 - playerIndex;
+                }
+            }
+
+            //Find all button and set the text to ""
+            //foreach (Button btn in this.Controls.OfType<Button>())
+            //{
+            //    btn.Text = "";
+            //}
+        }
+
+        string[] availableCells(string[] currentBoard)
+        {
+            List<string> availableCellsList = new List<string>();
+            for (int i = 0; i < currentBoard.Length; i++)
+            {
+                if (currentBoard[i] != "O" && currentBoard[i] != "X")
+                {
+                    availableCellsList.Add(i.ToString());
+                }
+            }
+            string[] availableCellsArray = availableCellsList.ToArray();
+            return availableCellsArray;
+        }
+
+        void startGame()
+        {
+            phase = gamePhase.gameStart;
+            if (checkAI.Checked)
+            {
+                if (playerIndex == 1)
+                {
+                    //Here comes AI
+                    int aiMove = minimax(playerIndex);
+                    if (aiMove != -1)
+                    {
+                        board[aiMove] = player[playerIndex];
+                        Button btn = this.Controls.Find("btn" + aiMove, false).FirstOrDefault() as Button;
+                        btn.Text = player[playerIndex];
+                        playerIndex = 1 - playerIndex;
+                    }
+                }
+            }
+        }
+
+        bool gameOver(string[] board, string player)
+        {
+            if (board[0] == player && board[1] == player && board[2] == player ||
+                board[3] == player && board[4] == player && board[5] == player ||
+                board[6] == player && board[7] == player && board[8] == player ||
+                board[0] == player && board[3] == player && board[6] == player ||
+                board[1] == player && board[4] == player && board[7] == player ||
+                board[2] == player && board[5] == player && board[8] == player ||
+                board[0] == player && board[4] == player && board[8] == player ||
+                board[2] == player && board[4] == player && board[6] == player
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        int getMax(string[] availableCellsArray)
+        {
+            double max = scoreIndex[int.Parse(availableCellsArray[0])];
+            int maxIndex = int.Parse(availableCellsArray[0]);
+            for (int i = 1; i < availableCellsArray.Length; i++)
+            {
+                if (scoreIndex[int.Parse(availableCellsArray[i])] > max)
+                {
+                    max = scoreIndex[int.Parse(availableCellsArray[i])];
+                    maxIndex = int.Parse(availableCellsArray[i]);
+                }
+            }
+            return maxIndex;
+        }
+
+        int minimax(int currentPlayerIndex)
+        {
+            int bestMove = -1;
+            string[] availableCellsArray = availableCells(board);
+
+            //Check all possible permutaion
+            List<string> permutation = new List<string>();
+            bool[] flag = new bool[board.Length];
+            
+            //initialize scoreIndex with 0
+            for (int i = 0; i < scoreIndex.Length; i++)
+            {
+                scoreIndex[i] = 0;
+            }
+
+            if (availableCellsArray.Length > 0)
+            {
+                //One more code to block diagonal move
+                if (board[0] == player[1 - playerIndex] && board[8] == player[1 - playerIndex] ||
+                    board[2] == player[1 - playerIndex] && board[6] == player[1 - playerIndex]
+                    )
+                {
+                    for (int i = 1; i < board.Length; i = i + 2)
+                    {
+                        if (availableCellsArray.Contains(i.ToString()))
+                        {
+                            bestMove = i;
+                        }
+                        else
+                        {
+                            Permute(availableCellsArray, permutation, flag, 1 - currentPlayerIndex);
+                            bestMove = getMax(availableCellsArray);
+                        }
+                    }
+                }
+                else
+                {
+                    Permute(availableCellsArray, permutation, flag, 1 - currentPlayerIndex);
+                    bestMove = getMax(availableCellsArray);
+                }
+            }
+            //if (availableCellsArray.Length > 0)
+            //{
+            //    Random rnd = new Random();
+            //    bestMove = int.Parse(availableCellsArray[rnd.Next(0, availableCellsArray.Length)]);
+            //}
+            return bestMove;
+        }
+
+
+        void Permute(string[] availableCellsArray, List<string> permutation, bool[] flag, int currentPlayerIndex)
+        {
+            if (permutation.Count == availableCellsArray.Length)
+            {
+
+                string[] tempBoard = new string[BOARD_SIZE];
+                
+                //copy the whole current board to tempBoard
+                for (int i = 0; i < BOARD_SIZE; i++)
+                {
+                    tempBoard[i] = board[i];
+                }
+
+                //Copy permutation to tempBoard
+                for (int i = 0; i < availableCellsArray.Length; i++)
+                {
+                    tempBoard[int.Parse(permutation[i])] = player[currentPlayerIndex];
+                    //If currrent player(AI) wins in this permutation, add 10 to the score of the first index of scoreIndex
+                    if (gameOver(tempBoard, player[currentPlayerIndex]))
+                    {
+                        scoreIndex[int.Parse(permutation[i])] += 10;
+                        return;
+                    }
+                    ////If another player(Player) wins in this permutation, subtract 10 from the score of the first index of scoreIndex
+                    else if (gameOver(tempBoard, player[1 - currentPlayerIndex]))
+                    {
+                        scoreIndex[int.Parse(permutation[i])] -= 10;
+                        return;
+                    }
+                    else
+                    {
+                        scoreIndex[int.Parse(permutation[i])] += 0;
+                    }
+                    currentPlayerIndex = 1 - currentPlayerIndex;
+                }
+
+                totalNumber++;
+            }
+
+            for (int i = 0; i < availableCellsArray.Length; i++)
+            {
+                //Skip if this flag is used
+                if (flag[i]) continue;
+
+                permutation.Add(availableCellsArray[i]);
+                //Set the flag true when it is used
+                flag[i] = true;
+
+                Permute(availableCellsArray, permutation, flag, currentPlayerIndex);
+                permutation.RemoveAt(permutation.Count - 1);
+                flag[i] = false;
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //Button works only when the phase is gameStart
+            if (phase != gamePhase.gameOver)
+            {
+                string btnName = (sender as Button).Name.Substring(3);
+                int btnIndex = int.Parse(btnName);
+
+                //Button works only when the button is not used by any players
+                if (board[btnIndex] != player[0] && board[btnIndex] != player[1])
+                {
+                    (sender as Button).Text = player[playerIndex];
+                    board[btnIndex] = player[playerIndex];
+
+                    if (gameOver(board, player[playerIndex]))
+                    {
+                        MessageBox.Show("Player " + (playerIndex + 1) + " WIN!!");
+                        phase = gamePhase.gameOver;
+                        return;
+                    }
+                    else
+                    {
+                        if (availableCells(board).Length == 0)
+                        {
+                            MessageBox.Show("DRAW...");
+                            phase = gamePhase.gameOver;
+                            return;
+                        }
+                        else
+                        {
+                            playerIndex = 1 - playerIndex;
+                            if (checkAI.Checked)
+                            {
+                                //Here comes AI
+                                int aiMove = minimax(playerIndex);
+                                if (aiMove != -1)
+                                {
+                                    board[aiMove] = player[playerIndex];
+                                    Button btn = this.Controls.Find("btn" + aiMove, false).FirstOrDefault() as Button;
+                                    btn.Text = player[playerIndex];
+                                    if (gameOver(board, player[playerIndex]))
+                                    {
+                                        MessageBox.Show("Player " + (playerIndex + 1) + " WIN!!");
+                                        phase = gamePhase.gameOver;
+                                        return;
+                                    }
+                                    else
+                                    {
+                                        if (availableCells(board).Length == 0)
+                                        {
+                                            MessageBox.Show("DRAW...");
+                                            phase = gamePhase.gameOver;
+                                            return;
+                                        }
+                                    }
+                                    playerIndex = 1 - playerIndex;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Board_Load(object sender, EventArgs e)
+        {
+            initialize();
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            initialize();
+        }
+
+        private void cBoxPlayer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            playerIndex = cBoxPlayer.SelectedIndex;
+            startGame();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
